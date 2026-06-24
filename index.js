95ca00b24300542bdbf9362eb1b375750f8f3fe2
Yashika <gyashika2555@gmail.com>
2026-06-24 15:12:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const employeeRoutes = require("./routes/employeeRoutes");
+const loggerMiddleware = require("./middleware/loggerMiddleware");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(loggerMiddleware);
+
+// Routes
+app.use("/employees", employeeRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Employee Management API Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server Running on Port 5000");
+});
