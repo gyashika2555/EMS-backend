90637d7692e36f3b8fffdb34e681a1387070057a
Yashika <gyashika2555@gmail.com>
2026-07-01 15:26:49 +0530

    added mongo code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const mongoose = require("mongoose");
+
 const employeeRoutes = require("./routes/employeeRoutes");
 const loggerMiddleware = require("./middleware/loggerMiddleware");
 
@@ -16,6 +18,15 @@ app.use("/employees", employeeRoutes);
 app.get("/", (req, res) => {
   res.send("Employee Management API Running");
 });
+
+mongoose.connect("mongodb+srv://yashikagoyal:yashikagoyal@cluster0.3jnya9v.mongodb.net/ems")
+.then(() =>{
+  console.log("Connected to MongoDB");
+})
+.catch((err) =>{
+  console.log(err);
+});
+
 
 app.listen(5000, () => {
   console.log("Server Running on Port 5000");
